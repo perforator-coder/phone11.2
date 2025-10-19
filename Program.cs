3c65bbd053458a1d23c80f4b9a85ed6366716cbc
perforator-coder <nicita.paschkovsky@mail.ru>
2025-10-19 11:00:15 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phone11._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            phone input_class = new phone();
+            Console.Write("Введите бренд телефона:");
+            string input_brand = Console.ReadLine();
+            Console.Write("Введите заряд телефона:");
+            if (int.TryParse(Console.ReadLine(), out int input_bat) == false) {
+                Console.WriteLine("Ошибка: Некорректый ввод.");
+                return;
+            }
+            input_class.Brand = input_brand;
+            input_class.Bat = input_bat;
+            input_class.Use();
+            input_class.Use();
+
+        }
+    }
+}
---
+++ phone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phone11._2
+{
+    internal class phone
+    {
+        private string brand;
+        private int bat;
+        private bool error_brand;
+        private bool error_bat;
+
+
+        public string Brand {
+            get { return brand; }
+            set {
+                if (String.IsNullOrWhiteSpace(value) == true)
+                {
+                    Console.WriteLine("Ошибка: бренд не может быть пустым.");
+                    error_brand = true;
+                }
+                else { 
+                    brand = value;
+                    error_brand = false;
+                }
+            }
+        }
+
+        public int Bat {
+            get { return bat; }
+            set {
+                if (value < 0 || value > 100) {
+                    Console.WriteLine("Ошибка:Значение батареи не в диапозоне.");
+                    error_bat = true;
+                } else
+                {
+                    bat = value;
+                    error_bat = false;
+                }
+            
+            }
+        }
+
+
+        public void Use() {
+            if (error_bat == false && error_brand == false && bat > 0)
+            {
+                Console.WriteLine($"Телефон {brand}, заряд {bat}.");
+                bat -= 10;
+            }
+            else if (error_brand == false && error_bat == false && bat <= 0) {
+                Console.WriteLine("Телефон разряжен!");
+            }
+        }
+    }
+}
